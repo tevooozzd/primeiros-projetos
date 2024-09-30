760b78982684442ee3231303592eef4ffdae48d5
tevoshw <estettv155@gmail.com>
2024-09-30 15:00:07 -0300

    entendendo_python01
    entendimento de criação e declarações de variáveis e uso do print()

---
+++ project1.py
@@ -0,0 +1,21 @@
+# - Criação de váriaveis 
+# - Letras maiúsculas e minúsculas fazem a diferença
+
+lojaum = 0
+lojaUm = 1
+print(lojaum, lojaUm) ## - Output = 0 1
+
+# -  Não se pode utilizar espaços
+# -  Para resolver isso utilizamos _, assim podemos juntar diferentes palavras
+# (Uma mensagem de erro aparece com a tentativa de variavel : 'loja um = 0'
+
+
+loja_um = 0
+print(loja_um)  ## - Output = 0
+
+# - Não se pode utilizar palavras chaves como variaveis (false, true, break, etc...)
+# - Output com mensagem de erro no exemplo: False = 'oi'
+
+
+
+
