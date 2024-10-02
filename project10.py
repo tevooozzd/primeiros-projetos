8e8faa953fcffd148550a439ae8dbbc49e8f2e09
tevoshw <estettv155@gmail.com>
2024-10-02 18:05:47 -0300

    Create project10.py
    I know this code is long, and could be summarized in a few lines, but I'm trying to understand and understand all of Python's features, and so in the future I will redo these projects in simple and clean ways!

---
+++ project10.py
@@ -0,0 +1,108 @@
+# ==================================================
+#          UNIT CONVERTER Project
+#          Using import time and sys
+#          Understanding methods in Python
+# ==================================================
+
+# Importing necessary libraries
+import time  # To add delays in the program
+import sys   # To exit the program
+
+# Welcome message and getting user's name
+print("================================================================================================")
+print("Welcome to the UNIT CONVERTER project!\nTo start, let's know your name...")
+print("================================================================================================")
+time.sleep(1.3)  # Pause for 1.3 seconds
+
+# Getting user's name
+name = input("Type your name here: ")  # Get the user's name
+print(f"So {name}, let's start")  # Greet the user
+print("================================================================================================")
+time.sleep(1.3)
+
+# Presenting options for unit conversion
+print("Type the number corresponding to the operation you want: \n"
+      "Length - 1\n"
+      "Temperature - 2\n"
+      "Capacity - 3\n"
+      "Mass - 4\n"
+      "Volume - 5\n")
+
+# Getting the user's choice for conversion operation
+convert_operation = input("What operation do you want to do?: ")
+while not convert_operation.isdigit() or not (1 <= int(convert_operation) <= 5):
+    print("This is not an option OR not a number! Please try again.")
+    time.sleep(0.8)
+    convert_operation = input("What operation do you want to do?: ")
+
+print(f"So {name}, you chose option {convert_operation}, let's start!")
+
+# Length conversion
+if convert_operation == '1':
+    print('==================================================================')
+    print("In this case of UNIT LENGTH, you will choose TWO (2) units. The first will be converted to the second. What do you prefer?")
+    print("Millimeter (mm) - 1\n"
+          "Centimeter (cm) - 2\n"
+          "Meter (m) - 3\n"
+          "Kilometer (km) - 4\n")
+    
+    # Getting user's choices for conversion
+    operationc1 = input("Choose one option to convert from: ")
+    operationc2 = input("Choose the second option to convert to: ")
+    value = float(input("Now, type the quantity you want to be converted: "))
+
+    # Validate that the two selected units are not the same
+    while operationc1 == operationc2 or value == '':
+        print("You can't use the same unit. Try again or enter a valid value!")
+        operationc1 = input("Choose one option to convert from: ")
+        operationc2 = input("Choose the second option to convert to: ")
+
+    # Perform the conversions based on user selections
+    if operationc1 == '1' and operationc2 == '2':
+        result = value / 10
+        print(f"So {name}, your operation result is {result} centimeters.")
+        sys.exit()
+    elif operationc1 == '1' and operationc2 == '3':
+        result = value / 1000
+        print(f"So {name}, your operation result is {result} meters.")
+        sys.exit()
+    elif operationc1 == '1' and operationc2 == '4':
+        result = value / 1000
+        print(f"So {name}, your operation result is {result} kilometers.")
+        sys.exit()
+    elif operationc1 == '2' and operationc2 == '1':
+        result = value * 10
+        print(f"So {name}, your operation result is {result} millimeters.")
+        sys.exit()
+    elif operationc1 == '2' and operationc2 == '3':
+        result = value / 100
+        print(f"So {name}, your operation result is {result} meters.")
+        sys.exit()
+    elif operationc1 == '2' and operationc2 == '4':
+        result = value / 100000
+        print(f"So {name}, your operation result is {result} kilometers.")
+        sys.exit()
+    elif operationc1 == '3' and operationc2 == '1':
+        result = value * 1000
+        print(f"So {name}, your operation result is {result} millimeters.")
+        sys.exit()
+    elif operationc1 == '3' and operationc2 == '2':
+        result = value * 100
+        print(f"So {name}, your operation result is {result} centimeters.")
+        sys.exit()
+    elif operationc1 == '3' and operationc2 == '4':
+        result = value / 1000
+        print(f"So {name}, your operation result is {result} kilometers.")
+        sys.exit()
+    elif operationc1 == '4' and operationc2 == '1':
+        result = value * 1000000
+        print(f"So {name}, your operation result is {result} millimeters.")
+        sys.exit()
+    elif operationc1 == '4' and operationc2 == '2':
+        result = value * 100000
+        print(f"So {name}, your operation result is {result} centimeters.")
+        sys.exit()
+    elif operationc1 == '4' and operationc2 == '3':
+        result = value / 1000
+        print(f"So {name}, your operation result is {result} meters.")
+        sys.exit()
