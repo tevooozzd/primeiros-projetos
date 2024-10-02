8f38d988dbd38aed4b5c8d52b94b37d643b076e6
tevoshw <estettv155@gmail.com>
2024-10-01 21:35:45 -0300

    Update project7.py
    just change the desing

--- project7.py
+++ project7.py
@@ -1,6 +1,6 @@
 
 # ================================
-#         IMPORTANT ALERT        #
+#         IMPORTANT ALERT
 # ================================
 
 
@@ -47,9 +47,10 @@ time.sleep(1)
 # Get the name
 name = input("Type your name: ")
 time.sleep(0.5)
-
+print('================================================')
 # Message to choose an operation
 print(f"So... Let's start {name}, choose an option to continue...")
+
 print("Type the number corresponding to the operation you want:\n"
     "Addition - 1\n"
     "Subtraction - 2\n"
@@ -57,40 +58,50 @@ print("Type the number corresponding to the operation you want:\n"
     "Division - 4\n"
     "Potentiation - 5")
 time.sleep(2)
-
+print('=============================================================')
 # Get user's choice
 to_choose = int(input("Type the operation by the number: "))
 
 # Perform chosen operation
 if to_choose == 1:
     print(f"So... you chose {to_choose}, let's start")
+    print('================================================')
     number_one = float(input("Type the first number you want to add: "))
     number_two = float(input("Type the second number you want to add: "))
     result = number_one + number_two
+    print('================================================')
     print(f"The addition of {number_one} + {number_two} is {result}")
 elif to_choose == 2:
     print(f"So... you chose {to_choose}, let's start")
+    print('================================================')
     number_one = float(input("Type the first number you want to subtract: "))
     number_two = float(input("Type the second number you want to subtract: "))
     result = number_one - number_two
+    print('================================================')
     print(f"The subtraction of {number_one} - {number_two} is {result}")
 elif to_choose == 3:
     print(f"So... you chose {to_choose}, let's start")
+    print('================================================')
     number_one = float(input("Type the first number you want to multiply: "))
     number_two = float(input("Type the second number you want to multiply: "))
     result = number_one * number_two
+    print('================================================')
     print(f"The multiplication of {number_one} * {number_two} is {result}")
 elif to_choose == 4:
     print(f"So... you chose {to_choose}, let's start")
+    print('================================================')
     number_one = float(input("Type the first number you want to divide: "))
     number_two = float(input("Type the second number you want to divide: "))
     result = number_one / number_two
+    print('================================================')
     print(f"The division of {number_one} / {number_two} is {result}")
 elif to_choose == 5:
     print(f"So... you chose {to_choose}, let's start")
+    print('================================================')
     number_one = float(input("Type the first number you want to raise: "))
     number_two = float(input("Type the second number you want to raise: "))
     result = number_one ** number_two
+    print('================================================')
     print(f"The potentiation of {number_one} ** {number_two} is {result}")
 else:
     print(f"Sorry {name}, that number isn't valid!")
