36ede54761b08030e826158ed5a59bf6c0e4d5a3
tevoshw <estettv155@gmail.com>
2024-10-03 20:20:41 -0300

    Update project11.py
    change the questions to a specific area

--- project11.py
+++ project11.py
@@ -1,30 +1,37 @@
-perguntas = {"Qual é a capital do Brasil" : "a",
-            "Qual é o maior planeta do sistema solar?": "b",
-            "Quem pintou a Mona Lisa?": "c",
-            "Qual é o continente conhecido como 'berço da civilização'?" : "c"
+perguntasmatematica = {"Qual é a raiz quadrada de 144?" : "c",
+        "Qual é a fórmula para calcular a área de um círculo?": "a",
+            "Se um triângulo tem lados de 3 cm, 4 cm e 5 cm, ele é um triângulo?": "a",
+            "Qual é o resultado de '15%' de 200?" : "b",
+            "Qual é a soma dos ângulos internos de um quadrado?" : "d",
+            "O que representa a hipotenusa em um triângulo retângulo?" : "a",
+            "Como se chama uma sequência de números em que cada número é a soma dos dois anteriores?" : "b",
+
             }
 
 
-opcoes  = {"Qual é a capital do Brasil" : "A) Brasília B) São Paulo C) Rio de Janeiro",
-            "Qual é o maior planeta do sistema solar?": "A) Terra B) Júpiter C)Marte",
-            "Quem pintou a Mona Lisa?": "A) Vincent van Gogh B) Pablo Picasso C) Leonardo da Vinci",
-            "Qual é o continente conhecido como 'berço da civilização'?" : "A) América B) Ásia C) África"
+opcoesmatematica  = {"Qual é a raiz quadrada de 144?" : "A) 10    B) 11    C) 12    D) 13",
+            "Qual é a fórmula para calcular a área de um círculo?": "A) A=πr    B) Y=mx + b     C) A + B + C    D) P = 4l   ",
+            "Se um triângulo tem lados de 3 cm, 4 cm e 5 cm, ele é um triângulo?": "A) Retângulo    B) Isóceles    C)   Escaleno    D) Quadrado",
+            "Qual é o resultado de '15%' de 200?" : "A) 60    B) 30    C) 15    D) 5.75",
+            "Qual é a soma dos ângulos internos de um quadrado?": " A)  40 graus   B) 50 graus   C) 180 graus  D) 360 graus ",
+            "O que representa a hipotenusa em um triângulo retângulo?": " A) maior lado do triângulo    B) Nada   C) O lado do ângulo   D) O menor lado",
+            "Como se chama uma sequência de números em que cada número é a soma dos dois anteriores?" : " A) Pitagoras   B) Sequência de Fibonacci C) Bhaskara  D) Algebra física",
             }
 import time
 acertos = 0
 erros = 0
 nperguntas = 0
-for pergunta in perguntas:
+for pergunta in perguntasmatematica:
     print(pergunta)
-    print(opcoes[pergunta])
+    print(opcoesmatematica[pergunta])
 
     chute = input("Qual alternativa é a resposta: ").lower()
 
-    while chute not in ['a', 'b', 'c']:
+    while chute not in ['a', 'b', 'c', 'd' ]:
         print("Essa não é uma opção!, vamos tentar novamente!")
         chute = input("Qual alternativa é a resposta: ").lower()
     
-    if chute == perguntas[pergunta]:
+    if chute == perguntasmatematica[pergunta]:
         print("Correto!")
         acertos += 1
         nperguntas += 1
