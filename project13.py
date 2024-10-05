ce113ae8bbd7d357ffd9ed0ccdd004f1ff566908
tevoshw <estettv155@gmail.com>
2024-10-05 17:07:01 -0300

    Update project13.py
    just use functions and logic progamming to min the code

--- project13.py
+++ project13.py
@@ -1,50 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
+import webbrowser
 
-print("Which of these cities do you want to know the time for?")
-print(" São Paulo - 1 (GMT -3)\n California - 2 (GMT -7)\n Tokyo - 3 (GMT +9)\n Berlin - 4 (GMT +2)")
-user_choice = int(input("Please enter your choice: "))
+
+print("Which country do you want to know about the timer (GMT)")
+user_choice = input("Please type the name of city: ")
 
 def time_info(choice):
-    if choice == 1:
-        selected_city = "Brasilia (GMT: -3)"
-        link = "https://www.google.com/search?q=current+time+in+brasilia"
-        header = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 OPR/113.0.0.0 (Edition std-2)"
-        }
-        request = requests.get(link, headers=header)
-        site = BeautifulSoup(request.text, "html.parser")
-        search_result = site.find("div", class_="YwPhnf")
-        print(f"Your selected city is {selected_city}, and the time is {search_result.get_text()}")
-    elif choice == 2:
-        selected_city = "California (GMT: -7)"
-        link = "https://www.google.com/search?q=current+time+in+california"
-        header = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 OPR/113.0.0.0 (Edition std-2)"
-        }
-        request = requests.get(link, headers=header)
-        site = BeautifulSoup(request.text, "html.parser")
-        search_result = site.find("div", class_="YwPhnf")
-        print(f"Your selected city is {selected_city}, and the time is {search_result.get_text()}")
-    elif choice == 3:
-        selected_city = "Tokyo (GMT: +9)"
-        link = "https://www.google.com/search?q=current+time+in+tokyo"
-        header = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 OPR/113.0.0.0 (Edition std-2)"
-        }
-        request = requests.get(link, headers=header)
-        site = BeautifulSoup(request.text, "html.parser")
-        search_result = site.find("div", class_="YwPhnf")
-        print(f"Your selected city is {selected_city}, and the time is {search_result.get_text()}")
-    else:
-        selected_city = "Berlin (GMT: +2)"
-        link = "https://www.google.com/search?q=current+time+in+berlin"
-        header = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 OPR/113.0.0.0 (Edition std-2)"
-        }
-        request = requests.get(link, headers=header)
-        site = BeautifulSoup(request.text, "html.parser")
-        search_result = site.find("div", class_="YwPhnf")
-        print(f"Your selected city is {selected_city}, and the time is {search_result.get_text()}")
+    link = f"https://www.google.com/search?q=time+in+{choice}"
+    header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 OPR/113.0.0.0 (Edition std-2)"                 }
+    requisição = requests.get(link, headers = header)
+    site = BeautifulSoup(requisição.text, "html.parser")
+
+    timer = site.find("div", class_ ="YwPhnf")
+    timerr = timer.get_text()
+
+
+    day_gmt = site.find("span", class_ = "KfQeJ")
+    day_gmtr = day_gmt.get_text()
+
+    country_zone = site.find("span", class_ = "vk_gy vk_sh")
+    country_zoner = country_zone.get_text()
+
+    print(f" {timerr}\n {day_gmtr}\n{country_zoner}")
+
+
+
 
 time_info(user_choice)
+
