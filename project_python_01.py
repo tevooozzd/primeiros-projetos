8d9de845b2dced6b78dac127db434b02c3ba6088
tevoshw <estettv155@gmail.com>
2024-10-06 20:05:44 -0300

    pjp01

---
+++ project_python_01.py
@@ -0,0 +1,49 @@
+
+
+name = input("Type your name: ")
+age = int(input("Type your age: "))
+rg_people = input("Type your registrations school: ")
+notes = []
+def welcome(nm, ag):
+    if ag >= 18:
+        print(f"Hello {name}, welcome to the calculate average project (college): ")
+    elif ag >= 15:
+        print(f"Hello {name}, welcome to the calculate average project (high-school): ")
+    else:
+        print(f"Hello {name}, welcome to the calculate average project (school): ")
+welcome(name, age)
+
+
+def registration(rg):
+    if rg[:2] != '00':
+
+        print(f"Welcome {name}, you're a student!")
+
+        for i in range(3):
+            notes_student = float(input("Type the note of the student: "))
+            notes.append(notes_student)
+    else:
+        print(f"Welcome {name}, you're a teacher!")
+
+        rg_student = input("Type the registration of the student: ")
+        print(f"Adding notes for student with registration {rg_student}.")
+        for i in range(3):
+            notes_student = float(input("Type the note of the student: "))
+            notes.append(notes_student)
+
+
+
+registration(rg_people)
+
+
+if notes:
+    average = sum(notes) / len(notes)
+    print(f"The average of the notes is: {average}")
+    if average >= 70:
+        print(f" CONGRATULATIONS, APRROVED! ")
+    else:
+        print(f"So sorry, didn't pass :(")
+else:
+    print("No notes were entered.")
+
+
